c5d59b398e0aa88ff9b3652470520bcca960a1a7
natali <shtegman.nata@yandex.ru>
2026-09-01 20:02:26 +0300

    upgrade LoginPage and LoginTest

--- src/test/java/org/example/PageOgjects/LoginPage.java
+++ src/test/java/org/example/PageOgjects/LoginPage.java
@@ -2,32 +2,35 @@ package org.example.PageOgjects;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class DemoQaLoginPage {
+public class LoginPage {
     private WebDriver driver;
     private WebDriverWait wait;
 
     private final By usernameLocator = By.id("username");
     private final By passwordLocator = By.id("password");
     private final By enterLocator = By.xpath("//button[@data-testid='login-submit']");
+    private final By errorMessageLocator = By.xpath("//div[@class='text-sm [&_p]:leading-relaxed']");
 
 
-    public DemoQaLoginPage(WebDriver driver, WebDriverWait wait) {
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
     }
 
     @Step("вход на сайт")
-    public DemoQaLoginPage open(){
+    public LoginPage open() {
         driver.get("https://demoqa.ru/bank/login");
         return this;
     }
+
     @Step("ввести имя пользователя")
-    public DemoQaLoginPage username(String username) {
+    public LoginPage username(String username) {
         WebElement name = wait.until(ExpectedConditions.elementToBeClickable(usernameLocator));
         name.clear();
         name.sendKeys(username);
@@ -35,7 +38,7 @@ public class DemoQaLoginPage {
     }
 
     @Step("ввести пароль")
-    public DemoQaLoginPage password(String pass) {
+    public LoginPage password(String pass) {
         WebElement password = driver.findElement(passwordLocator);
         password.clear();
         password.sendKeys(pass);
@@ -43,7 +46,7 @@ public class DemoQaLoginPage {
     }
 
     @Step("нажать на кнопку войти")
-    public DemoQaLoginPage enterButton(){
+    public LoginPage enterButton() {
         WebElement button = wait.until(ExpectedConditions.elementToBeClickable(enterLocator));
         button.click();
         return this;
@@ -51,13 +54,32 @@ public class DemoQaLoginPage {
     }
 
     @Step("проверка логина")
-    public String getUsernameValue(){
+    public String getUsernameValue() {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(usernameLocator)).getAttribute("value");
     }
 
     @Step("проверка пароля")
-    public String getPasswordValue(){
+    public String getPasswordValue() {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(passwordLocator)).getAttribute("value");
     }
 
+    @Step("проверка выскакивания сообщения об ошибке")
+    public boolean isErrorMessageDisplayed() {
+        try {
+            WebElement result = wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessageLocator));
+            return result.isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    @Step("проверка захода на сайт")
+    public boolean isEnterSiteSuccess(String site) {
+        try {
+            wait.until(ExpectedConditions.urlToBe(site));
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
 }
--- src/test/java/org/example/Tests/LoginPageTest.java
+++ src/test/java/org/example/Tests/LoginPageTest.java
@@ -3,33 +3,51 @@ package org.example.Tests;
 
 import io.qameta.allure.*;
 import org.example.BaseTest;
-import org.example.PageOgjects.DemoQaLoginPage;
+import org.example.PageOgjects.LoginPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @Epic("Элементы интерфейса")
 @Feature("Взаимодействие с сайтом")
 public class LoginPageTest extends BaseTest {
-    private DemoQaLoginPage loginPage;
+    private LoginPage loginPage;
+
     @BeforeEach
-    public void setupPage(){
-        loginPage = new DemoQaLoginPage(getDriver(),getWait());
+    public void setupPage() {
+        loginPage = new LoginPage(getDriver(), getWait());
     }
 
     @Test
     @Story("Удачный вход в банк")
-    @DisplayName("вход в банк по логину и паролю")
+    @DisplayName("вход в банк по правильному логину и паролю")
     @Severity(SeverityLevel.CRITICAL)
-    public void loginTest(){
+    public void loginTest() {
 
         loginPage.open()
                 .username("testuser")
                 .password("password")
                 .enterButton();
-        Assertions.assertEquals("testuser",loginPage.getUsernameValue());
-        Assertions.assertEquals("password",loginPage.getPasswordValue());
+Assertions.assertTrue(loginPage.isEnterSiteSuccess("https://demoqa.ru/bank"),"Переход в личный кабинет не выполнен после ввода верных данных");
     }
+
+    @ParameterizedTest(name = "Запуск #{index}: Проверка с паролем [{0}]")
+    @ValueSource(strings = {"wrongpassword", "  "})
+    @Story(" вход в банк с неправильным паролем")
+    @DisplayName("вход в банк с неправильным паролем")
+    @Severity(SeverityLevel.CRITICAL)
+    public void negativeLoginTest(String wrongPassword) {
+
+        loginPage.open()
+                .username("testuser")
+                .password(wrongPassword)
+                .enterButton();
+        Assertions.assertTrue(loginPage.isErrorMessageDisplayed(), "Сообщение об ошибке не появилось после ввода неверного пароля");
+
+    }
+
 
 }
