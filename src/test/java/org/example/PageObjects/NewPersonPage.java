e553c9574b506f8c7603f1935b293f746506a62e
natali <shtegman.nata@yandex.ru>
2026-09-12 12:10:34 +0300

    upgrade NewPersonTest and NewPersonPage

---
+++ src/test/java/org/example/PageObjects/NewPersonPage.java
@@ -0,0 +1,119 @@
+package org.example.PageObjects;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NewPersonPage {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    private final By firstNameLocator = By.id("firstName");
+    private final By lastNameLocator = By.id("lastName");
+    private final By emailLocator = By.id("email");
+    private final By usernameLocator = By.id("username");
+    private final By passwordLocator = By.id("password");
+    private final By confirmPasswordLocator = By.id("confirmPassword");
+    private final By registerButtonLocator =
+            By.xpath("//button[@data-testid = 'register-submit']");
+    private final By PasswordDontMatchLocator = By.xpath("//*[@data-testid='register-confirm-password-error']");
+
+    public NewPersonPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    @Step("проверка активности кнопки зарегистрироваться")
+    public boolean isRegisterButtonDisplayed() {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(registerButtonLocator));
+        return !element.isEnabled();
+    }
+    @Step("проверка что пароли не совпадают")
+    public boolean isPasswordDontMatch() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(PasswordDontMatchLocator));
+        return true;
+    }
+    @Step("Открытие страницы регистрации")
+    public NewPersonPage site(String site) {
+        driver.get(site);
+        wait.until(ExpectedConditions.urlToBe(site));
+        return this;
+    }
+
+    @Step("Ввод имени пользователя")
+    public NewPersonPage firstName(String firstName) {
+        WebElement element = wait.until(
+                ExpectedConditions.elementToBeClickable(firstNameLocator));
+        element.clear();
+        element.sendKeys(firstName);
+        return this;
+    }
+
+    @Step("Ввод фамилии пользователя")
+    public NewPersonPage lastName(String lastName) {
+        WebElement element = wait.until(
+                ExpectedConditions.elementToBeClickable(lastNameLocator));
+        element.clear();
+        element.sendKeys(lastName);
+        return this;
+    }
+
+    @Step("Ввод email")
+    public NewPersonPage email(String email) {
+        WebElement element = wait.until(
+                ExpectedConditions.elementToBeClickable(emailLocator));
+        element.clear();
+        element.sendKeys(email);
+        return this;
+    }
+
+    @Step("Ввод имени пользователя")
+    public NewPersonPage username(String username) {
+        WebElement element = wait.until(
+                ExpectedConditions.elementToBeClickable(usernameLocator));
+        element.clear();
+        element.sendKeys(username);
+        return this;
+    }
+
+    @Step("Ввод пароля")
+    public NewPersonPage password(String password) {
+        WebElement element = wait.until(
+                ExpectedConditions.elementToBeClickable(passwordLocator));
+        element.clear();
+        element.sendKeys(password);
+        return this;
+    }
+
+    @Step("Ввод подтверждения пароля")
+    public NewPersonPage confirmPassword(String confirmPassword) {
+        WebElement element = wait.until(
+                ExpectedConditions.elementToBeClickable(confirmPasswordLocator));
+        element.clear();
+        element.sendKeys(confirmPassword);
+        return this;
+    }
+
+    @Step("Нажатие кнопки регистрации")
+    public NewPersonPage registerButton() {
+        WebElement element = wait.until(
+                ExpectedConditions.elementToBeClickable(registerButtonLocator));
+        element.click();
+        return this;
+    }
+
+    @Step("Проверка успешной регистрации")
+    public boolean isRegistrationSuccess(String site) {
+        return wait.until(ExpectedConditions.urlToBe(site));
+    }
+
+    @Step("Проверка неуспешной регистрации")
+    public boolean isRegistrationFail(String site) {
+        return !driver.getCurrentUrl().equals(site);
+    }
+
+}
--- src/test/java/org/example/Tests/NewPersonTest.java
+++ src/test/java/org/example/Tests/NewPersonTest.java
@@ -2,36 +2,177 @@ package org.example.Tests;
 
 import io.qameta.allure.*;
 import org.example.BaseTest;
-import org.example.PageOgjects.NewPersonPage;
+import org.example.PageObjects.NewPersonPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@Epic("регистрация пользователя")
-@Feature("регистрация")
+@Epic("Регистрация пользователя")
+@Feature("Регистрация")
 public class NewPersonTest extends BaseTest {
-private NewPersonPage newPersonPage;
 
-@BeforeEach
-public void setUp() {
-    newPersonPage = new NewPersonPage(getDriver(),getWait());
-}
+    private NewPersonPage newPersonPage;
 
-@Test
-@Story("удачная регистрация пользователя")
-@DisplayName("регистрация нового пользователя")
-@Severity(SeverityLevel.BLOCKER)
-    public void newPersonTest(){
-    newPersonPage.site("https://demoqa.ru/bank/register");
-   newPersonPage.firstName("IvanTestUser")
-           .lastName("IvanovTestUser")
-           .email("test@mail.ru")
-           .username("test")
-           .password("testpassword")
-           .confirmPassword("testpassword")
-           .registerButton();
-    Assertions.assertTrue(newPersonPage.isEnterSiteSuccess("https://demoqa.ru/bank"));
-}
+    @BeforeEach
+    public void setUp() {
+        newPersonPage = new NewPersonPage(getDriver(), getWait());
+    }
 
+    @Test
+    @Story("Удачная регистрация пользователя")
+    @DisplayName("Регистрация нового пользователя")
+    @Severity(SeverityLevel.CRITICAL)
+    public void newPersonSuccessTest() {
+
+        newPersonPage.site("https://demoqa.ru/bank/register")
+                .firstName("IvanTestUser")
+                .lastName("IvanovTestUser")
+                .email("test@mail.ru")
+                .username("test")
+                .password("testpassword")
+                .confirmPassword("testpassword")
+                .registerButton();
+
+        Assertions.assertTrue(
+                newPersonPage.isRegistrationSuccess("https://demoqa.ru/bank"),
+                "Пользователь не был зарегистрирован");
+    }
+
+
+    @Test
+    @Story("неуспешная регистрация пользователя без имени")
+    @DisplayName("Регистрация нового пользователя без имени")
+    @Severity(SeverityLevel.CRITICAL)
+    public void registrationWithoutFirstNameTest() {
+        newPersonPage
+                .site("https://demoqa.ru/bank/register")
+                .firstName("")
+                .lastName("Ivanov")
+                .email("test@mail.ru")
+                .username("user1")
+                .password("pass123")
+                .confirmPassword("pass123")
+                .registerButton();
+        Assertions.assertTrue(
+                newPersonPage.isRegistrationFail("https://demoqa.ru/bank"),
+                "Регистрация прошла успешно");
+    }
+
+    @Test
+    @Story("Неуспешная регистрация пользователя без фамилии")
+    @DisplayName("Регистрация нового пользователя без фамилии")
+    @Severity(SeverityLevel.CRITICAL)
+    public void registrationWithoutLastNameTest() {
+        newPersonPage
+                .site("https://demoqa.ru/bank/register")
+                .firstName("Ivan")
+                .lastName("")
+                .email("test@mail.ru")
+                .username("user1")
+                .password("pass123")
+                .confirmPassword("pass123")
+                .registerButton();
+        Assertions.assertTrue(
+                newPersonPage.isRegistrationFail("https://demoqa.ru/bank"),
+                "Регистрация прошла успешно");
+    }
+
+    @Test
+    @Story("неуспешная регистрация пользователя без имейла")
+    @DisplayName("Регистрация нового пользователя без имейла")
+    @Severity(SeverityLevel.CRITICAL)
+    public void registrationWithoutEmailTest() {
+        newPersonPage
+                .site("https://demoqa.ru/bank/register")
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .email("")
+                .username("user1")
+                .password("pass123")
+                .confirmPassword("pass123")
+                .registerButton();
+        Assertions.assertTrue(
+                newPersonPage.isRegistrationFail("https://demoqa.ru/bank"),
+                "Регистрация прошла успешно");
+    }
+
+    @Test
+    @Story("неуспешная регистрация пользователя без имейла")
+    @DisplayName("Регистрация нового пользователя без имейла")
+    @Severity(SeverityLevel.CRITICAL)
+    public void registrationWithWrongEmailTest() {
+        newPersonPage
+                .site("https://demoqa.ru/bank/register")
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .email("test.tu")
+                .username("user1")
+                .password("pass123")
+                .confirmPassword("pass123")
+                .registerButton();
+        Assertions.assertTrue(
+                newPersonPage.isRegistrationFail("https://demoqa.ru/bank"),
+                "Регистрация прошла успешно");
+    }
+
+    @Test
+    @Story("Неуспешная регистрация пользователя без логина")
+    @DisplayName("Регистрация нового пользователя без логина")
+    @Severity(SeverityLevel.CRITICAL)
+    public void registrationWithoutUsernameTest() {
+        newPersonPage
+                .site("https://demoqa.ru/bank/register")
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .email("test@mail.ru")
+                .username("")
+                .password("pass123")
+                .confirmPassword("pass123")
+                .registerButton();
+        Assertions.assertTrue(
+                newPersonPage.isRegistrationFail("https://demoqa.ru/bank"),
+                "Регистрация прошла успешно");
+    }
+
+    @Test
+    @Story("Неуспешная регистрация пользователя без пароля")
+    @DisplayName("Регистрация нового пользователя без пароля")
+    @Severity(SeverityLevel.CRITICAL)
+    public void registrationWithoutPasswordTest() {
+        newPersonPage
+                .site("https://demoqa.ru/bank/register")
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .email("test@mail.ru")
+                .username("user1")
+                .password("")
+                .confirmPassword("")
+                .registerButton();
+        Assertions.assertTrue(
+                newPersonPage.isRegistrationFail("https://demoqa.ru/bank"),
+                "Регистрация прошла успешно");
+    }
+
+    @Test
+    @Story("Неуспешная регистрация пользователя без пароля")
+    @DisplayName("Регистрация нового пользователя без пароля")
+    @Severity(SeverityLevel.CRITICAL)
+    public void registrationWithoutSamePasswordTest() {
+        newPersonPage
+                .site("https://demoqa.ru/bank/register")
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .email("test@mail.ru")
+                .username("user1")
+                .password("1234")
+                .confirmPassword("4321");
+
+        Assertions.assertTrue(newPersonPage.isPasswordDontMatch(),"Ошибка о несовпадении паролей не появилась");
+        Assertions.assertTrue(newPersonPage.isRegisterButtonDisplayed(), "Кнопка регистрации осталась активной!");
+        Assertions.assertTrue(
+                newPersonPage.isRegistrationFail("https://demoqa.ru/bank"),
+                "Регистрация прошла успешно");
+
+    }
 }
